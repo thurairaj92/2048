c2e4ec37980fdad3a9b47cbe16fb210af1c51025
thurairaj92 <thurairaj92@gmail.com>
2014-03-24 21:57:26 -0400

    Adding files

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "Go get a life!" : "Try again, BITCH!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
